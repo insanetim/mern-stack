710efb074d6a16225fc8154bdda0a0f8cfc2b4ce
insanetim <47120724+insanetim@users.noreply.github.com>
2024-02-12 10:40:21 +0200

    added client

--- server.js
+++ server.js
@@ -21,22 +21,16 @@ app.use(express.json())
 
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.use('/', require('./routes/root'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/notes', require('./routes/noteRoutes'))
 
-app.all('*', (req, res) => {
-  res.status(404)
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
-  } else if (req.accepts('json')) {
-    res.json({ message: '404 Not Found' })
-  } else {
-    res.type('text').send('404 Not Found')
-  }
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.all('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.use(errorHandler)
 
