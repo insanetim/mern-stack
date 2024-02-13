2342e262cbb9d55710406abde7e89cd194d759bc
insanetim <47120724+insanetim@users.noreply.github.com>
2024-02-13 23:18:51 +0200

    update allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://mern-stack-insanetim.onrender.com/',
+  'https://mern-stack-insanetim.onrender.com',
 ]
 
 module.exports = allowedOrigins
